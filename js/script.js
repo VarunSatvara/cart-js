72fcb54a99085e6376f8b6bdf0e55551eeec4c94
varun satvara <varunsatvara1997@gmail.com>
2020-01-23 18:34:37 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function cartadd() {
+var img1 = document.getElementById('img1').getAttribute("src");
 
+
+function add() {
+    var item1 = document.getElementById("item1");
+    var cartimg1 = document.createElement('img');
+    cartimg1.setAttribute("src", img1);
+    cartimg1.setAttribute("height", "50px");
+    item1.appendChild(cartimg1);
 }
