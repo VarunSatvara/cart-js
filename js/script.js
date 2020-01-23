ff1bb7568b4e0c3eb5c221238891626ce9ca0ca2
varun satvara <varunsatvara1997@gmail.com>
2020-01-23 19:04:01 +0530

    update

--- js/script.js
+++ js/script.js
@@ -7,4 +7,16 @@ function add() {
     cartimg1.setAttribute("src", img1);
     cartimg1.setAttribute("height", "50px");
     item1.appendChild(cartimg1);
+
+    var item2 = document.getElementById("item2");
+    var food1 = document.getElementById('food1');
+    item2.appendChild(food1);
+
+    var item3 = document.getElementById("item3");
+    var price1 = document.getElementById('price1');
+    item3.appendChild(price1);
+}
+
+function remove() {
+    document.getElementById('item1').removeAttribute("onclick");
 }
