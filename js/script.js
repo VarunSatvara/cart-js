ec7721862acb591ce4ea42617e63179aa4f9e13a
varun satvara <varunsatvara1997@gmail.com>
2020-01-24 17:47:28 +0530

    update

--- js/script.js
+++ js/script.js
@@ -4,16 +4,14 @@ function add(item) {
     var item1 = document.getElementById("item" + item);
     console.log(item1);
 
-
-
-    var img = document.getElementById('img' + item);
+    var img = document.getElementById('img' + item).getAttribute("src");
     console.log(img);
 
     var price = document.getElementById('price' + item);
-    console.log(price);
+    price.innerHTML;
+    console.log(price.innerHTML);
 
     var food = document.getElementById('food' + item);
-    console.log(food);
-
-
+    food.innerHTML;
+    console.log(food.innerHTML);
 }
