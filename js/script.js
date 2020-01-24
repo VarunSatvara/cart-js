bf5baf9d30db102287b53e5a5110f5f0e22b150d
varun satvara <varunsatvara1997@gmail.com>
2020-01-24 15:16:49 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-var img1 = document.getElementById('img1').getAttribute("src");
+function add(item) {
+    console.log(item);
+
+    var item1 = document.getElementById("item" + item);
+    console.log(item1);
 
 
-function add() {
-    var item1 = document.getElementById("item1");
-    var cartimg1 = document.createElement('img');
-    cartimg1.setAttribute("src", img1);
-    cartimg1.setAttribute("height", "50px");
-    item1.appendChild(cartimg1);
 
-    var item2 = document.getElementById("item2");
-    var food1 = document.getElementById('food1');
-    item2.appendChild(food1);
+    var img = document.getElementById('img' + item);
+    console.log(img);
 
-    var item3 = document.getElementById("item3");
-    var price1 = document.getElementById('price1');
-    item3.appendChild(price1);
-}
+    var price = document.getElementById('price' + item);
+    console.log(price);
 
-function remove() {
-    document.getElementById('item1').removeAttribute("onclick");
+    var food = document.getElementById('food' + item);
+    console.log(food);
+
+
 }
