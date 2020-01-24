1a75d92c4655ad62b4ed5a7d9c07aae339fe452b
varun satvara <varunsatvara1997@gmail.com>
2020-01-24 19:47:53 +0530

    upadte

--- js/script.js
+++ js/script.js
@@ -2,16 +2,28 @@ function add(item) {
     console.log(item);
 
     var item1 = document.getElementById("item" + item);
-    console.log(item1);
+
 
     var img = document.getElementById('img' + item).getAttribute("src");
-    console.log(img);
 
     var price = document.getElementById('price' + item);
     price.innerHTML;
-    console.log(price.innerHTML);
+
 
     var food = document.getElementById('food' + item);
     food.innerHTML;
-    console.log(food.innerHTML);
+
+
+
+    var list = document.getElementById("list");
+
+    var ti = document.createElement('li');
+
+    console.log(ti);
+    ti.innerHTML = '<div class="row"><div class="col-4"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-4">vegatble food</div><div class="col-4">$15</div></div>';
+
+
+    list.appendChild(ti);
+
+
 }
