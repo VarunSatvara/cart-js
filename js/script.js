275dd6a4288245cd9bc2f76bb5bfbcbae5f3fb41
varun satvara <varunsatvara1997@gmail.com>
2020-01-25 15:19:14 +0530

    update

--- js/script.js
+++ js/script.js
@@ -13,12 +13,10 @@ function add(item) {
     var food = document.getElementById('food' + item);
     food.innerHTML;
 
-
-
-
     var list = document.getElementById("list");
 
     var ti = document.createElement('li');
+    ti.classList.add("p-2");
     console.log(ti);
 
     ti.innerHTML = '<div class="row"><div class="col-4"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-4 ">' + food.innerHTML + '</div><div class="col-4">' + price.innerHTML + '</div></div>';
