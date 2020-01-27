a96c287da18cade48f56bd2bf3cd013efaf12648
varun satvara <varunsatvara1997@gmail.com>
2020-01-27 14:18:59 +0530

    update

--- js/script.js
+++ js/script.js
@@ -19,10 +19,16 @@ function add(item) {
     ti.classList.add("p-2");
     console.log(ti);
 
-    ti.innerHTML = '<div class="row"><div class="col-4"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-4 ">' + food.innerHTML + '</div><div class="col-4">' + price.innerHTML + '</div></div>';
+    ti.innerHTML = '<div class="row"><div class="col-3"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-3 ">' + food.innerHTML + '</div><div class="col-3">' + price.innerHTML + '</div><div class="col-3"><i class="fas fa-minus-circle minus" onclick="remove"></i></div></div>';
 
 
     list.appendChild(ti);
 
 
+}
+
+function remove() {
+    var myobj = document.getElementById("item");
+    myobj.remove();
+    console.log(myobj);
 }
