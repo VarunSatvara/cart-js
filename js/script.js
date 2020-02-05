9c9196881e25b1d83b639a731f99b1cd4b5e2ebe
varun satvara <varunsatvara1997@gmail.com>
2020-02-05 15:31:09 +0530

    update

--- js/script.js
+++ js/script.js
@@ -3,27 +3,27 @@ function add(item) {
 
     var item1 = document.getElementById("item" + item);
 
-
     var img = document.getElementById('img' + item).getAttribute("src");
 
     var price = document.getElementById('price' + item);
     price.innerHTML;
-
 
     var food = document.getElementById('food' + item);
     food.innerHTML;
 
     var list = document.getElementById("list");
 
+
     var ti = document.createElement('li');
     ti.classList.add("p-2");
-    console.log(ti);
+
 
     ti.onclick = function() {
         this.parentNode.removeChild(this);
     }
 
-    ti.innerHTML = '<div class="row"><div class="col-3"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-3 ">' + food.innerHTML + '</div><div class="col-3">' + price.innerHTML + '</div><div class="col-3"><button onlclick="remove()">r</button></div></div>';
+
+    ti.innerHTML = '<div class="row"><div class="col-3"> <img src="' + img + '" class="img-fluid fd"></div><div class="col-3 ">' + food.innerHTML + '</div><div class="col-3">' + price.innerHTML + '</div><div class="col-3"><i class="fas fa-minus-circle"></i></div></div>';
 
     list.appendChild(ti);
 }
